c038db31e1376001910247d7f307d7c4c24e29d7
levitus-Kt <fetdenal@ya.ru>
2025-08-04 14:28:27 +0300

    feat: feat client and serevr

---
+++ client.py
@@ -0,0 +1,9 @@
+import socket
+
+client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_sock.connect(('127.0.0.1', 53210))
+print("Connected")
+client_sock.sendall(b'Hello, world')
+data = client_sock.recv(1024)
+client_sock.close()
+print('Received', repr(data))
