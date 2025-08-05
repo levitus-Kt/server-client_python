d5afc344ad75c3fd951215bf5932b82846693cbc
levitus-Kt <fetdenal@ya.ru>
2025-08-05 14:45:35 +0300

    feat(core): feat udp server-client

--- src/client.py
+++ src/client.py
@@ -4,19 +4,20 @@ import os
 import _io
 
 # Создаем сокет клиента
-client_sock: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_sock: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Получаем хост локальной машины
 hostname: str = socket.gethostname()
 # Устанавливаем порт сервера                                  
 port: int = 53210     
 # Подключаемся к серверу                                                   
-client_sock.connect((hostname, port))
-print("Connected")
+serv_addr: tuple = (hostname, port)
 
 # Получаем файл для запроса с сервера
 if sys.argv[1]: file_from_server: str = os.path.basename(sys.argv[1])
 # Отправляем данные серверу
-client_sock.send(file_from_server.encode())
+# sock.sendto(b"GET_FILE", (host, port))
+data: bytes = file_from_server.encode()
+client_sock.sendto(data, serv_addr)
 
 print(f"requesting from {hostname}:{port}")
 print("downloading...")
@@ -26,12 +27,16 @@ newfile: _io.TextIOWrapper = open("newfile", "w")
 
 while True:
     # получаем данные от сервера
-    data: bytes = client_sock.recv(1024)
+    data, addr = client_sock.recvfrom(1024)
     # декодируем данные и записываем в файл
     newfile.write(bytes.decode(data))
     # файл закончился
-    if not data: break
+    message = input('Please type a word ')
+    if message == 'stop' or message == '':
+        newfile.close()  # Закрываем файл
+        client_sock.close()
+        break
 
-newfile.close()  # Закрываем файл
+
 client_sock.close()  # Закрываем подключение
 print('downloaded as', os.path.abspath("newfile"))
--- src/server.py
+++ src/server.py
@@ -6,23 +6,18 @@ import _io
 # Создаем объект сокета сервера
 serv_sock: socket.socket = socket.socket(
     socket.AF_INET, 
-    socket.SOCK_STREAM, 
-    proto=0)
+    socket.SOCK_DGRAM)
 # Получаем имя хоста локальной машины
 hostname: str = socket.gethostname()
 # Устанавливаем порт сервера
 port: int = 53210
 # Привязываем сокет сервера к хосту и порту
 serv_sock.bind((hostname, port))
-# Начинаем прослушиваение входящих подключений
-serv_sock.listen(10)
 # Получаем файл для отправки клиенту
 if sys.argv[1]: file_to_client = sys.argv[1]
 
-# Принимаем клиента
-client_sock, client_addr = serv_sock.accept()
 # Получаем данные от клиента
-data: bytes = client_sock.recv(1024)
+client_sock, client_addr = serv_sock.recvfrom(1024)
 
 print("serving", os.path.abspath(file_to_client))
 print('request from', client_addr[0] + ":" + str(client_addr[1]))
@@ -34,11 +29,10 @@ print("sending data to client...")
 # Считываем данные из файла блоками по 1024 байт и отправляем клиенту
 line: bytes = file.read(1024)
 while line:
-    client_sock.send(line) # отправляем строку клиенту
+    serv_sock.sendto(line, client_addr) # отправляем строку клиенту
     line = file.read(1024)
 
 print(f"finished sending to", client_addr[0] + ":" + str(client_addr[1]))
 
 file.close()         # закрываем файл
-client_sock.close()  # закрываем клиента
 serv_sock.close()    # закрываем сервер
