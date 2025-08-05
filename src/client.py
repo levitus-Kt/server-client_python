af1265641ad86c366059c6debb80f626d19649f8
levitus-Kt <fetdenal@ya.ru>
2025-08-05 13:41:34 +0300

    style(core): Add type hints

--- src/client.py
+++ src/client.py
@@ -1,24 +1,37 @@
 import socket
 import sys
 import os
+import _io
 
-client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)     # создаем сокет клиента
-hostname = socket.gethostname()                                     # получаем хост локальной машины
-port = 53210                                                        # устанавливаем порт сервера
-client_sock.connect((hostname, port))                   # подключаемся к серверу
+# Создаем сокет клиента
+client_sock: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Получаем хост локальной машины
+hostname: str = socket.gethostname()
+# Устанавливаем порт сервера                                  
+port: int = 53210     
+# Подключаемся к серверу                                                   
+client_sock.connect((hostname, port))
 print("Connected")
-if sys.argv[1]: file_from_server = sys.argv[1]                      # получаем файл для запроса с сервера
-file_from_server = os.path.basename(file_from_server)               # получаем название файла, если был указан путь
-client_sock.send(file_from_server.encode())       # отправляем данные серверу
+
+# Получаем файл для запроса с сервера
+if sys.argv[1]: file_from_server: str = os.path.basename(sys.argv[1])
+# Отправляем данные серверу
+client_sock.send(file_from_server.encode())
+
 print(f"requesting from {hostname}:{port}")
 print("downloading...")
-newfile = open("newfile", "w")
+
+# Создаем файл для записи
+newfile: _io.TextIOWrapper = open("newfile", "w")
 
 while True:
-    data = client_sock.recv(1024)    # получаем данные от сервера
-    newfile.write(bytes.decode(data))   # декодируем данные и записываем в файл
-    if not data: break              # файл закончился
+    # получаем данные от сервера
+    data: bytes = client_sock.recv(1024)
+    # декодируем данные и записываем в файл
+    newfile.write(bytes.decode(data))
+    # файл закончился
+    if not data: break
 
-newfile.close()
-client_sock.close()                     # закрываем подключение
+newfile.close()  # Закрываем файл
+client_sock.close()  # Закрываем подключение
 print('downloaded as', os.path.abspath("newfile"))
--- src/server.py
+++ src/server.py
@@ -1,32 +1,44 @@
 import socket
 import sys
 import os
+import _io
 
-serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)  # создаем объект сокета сервера
-hostname = socket.gethostname()                  # получаем имя хоста локальной машины
-port = 53210                            # устанавливаем порт сервера
-serv_sock.bind((hostname, port))        # привязываем сокет сервера к хосту и порту
-serv_sock.listen(10)                    # начинаем прослушиваение входящих подключений
-if sys.argv[1]: file_to_client = sys.argv[1]    # получаем файл для отправки клиенту
+# Создаем объект сокета сервера
+serv_sock: socket.socket = socket.socket(
+    socket.AF_INET, 
+    socket.SOCK_STREAM, 
+    proto=0)
+# Получаем имя хоста локальной машины
+hostname: str = socket.gethostname()
+# Устанавливаем порт сервера
+port: int = 53210
+# Привязываем сокет сервера к хосту и порту
+serv_sock.bind((hostname, port))
+# Начинаем прослушиваение входящих подключений
+serv_sock.listen(10)
+# Получаем файл для отправки клиенту
+if sys.argv[1]: file_to_client = sys.argv[1]
 
-while True:
-    # Бесконечно обрабатываем входящие подключения
-    client_sock, client_addr = serv_sock.accept()   # принимаем клиента
-    data = client_sock.recv(1024)           # получаем данные от клиента
-    print("serving", os.path.abspath(file_to_client))
-    print('request from', client_addr[0] + ":" + str(client_addr[1]))
+# Принимаем клиента
+client_sock, client_addr = serv_sock.accept()
+# Получаем данные от клиента
+data: bytes = client_sock.recv(1024)
 
-    file = open(file_to_client, "rb")     # открываем файл для отправки
-    print("sending data to client...")
+print("serving", os.path.abspath(file_to_client))
+print('request from', client_addr[0] + ":" + str(client_addr[1]))
 
-    # считываем данные из файла блоками по 1024 байт и отправляем клиенту
+# Открываем файл для отправки
+file: _io.BufferedReader = open(file_to_client, "rb")     
+print("sending data to client...")
+
+# Считываем данные из файла блоками по 1024 байт и отправляем клиенту
+line: bytes = file.read(1024)
+while line:
+    client_sock.send(line) # отправляем строку клиенту
     line = file.read(1024)
-    while line:
-        client_sock.send(line)      # отправляем строку клиенту 
-        line = file.read(1024)
-    
-    print(f"finished sending to", client_addr[0] + ":" + str(client_addr[1]))
 
-    file.close()            # закрываем файл
-    client_sock.close()     # закрываем клиента
-    serv_sock.close()       # закрываем сервер
+print(f"finished sending to", client_addr[0] + ":" + str(client_addr[1]))
+
+file.close()         # закрываем файл
+client_sock.close()  # закрываем клиента
+serv_sock.close()    # закрываем сервер
